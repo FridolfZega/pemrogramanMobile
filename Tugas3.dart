9fdf5ba8ff0ba3fc7a05c19cabaf49a859c2f7cd
FridolfZega <146351658+FridolfZega@users.noreply.github.com>
2025-11-02 21:53:52 +0700

    nambah tugas 3

---
+++ Tugas3.dart
@@ -0,0 +1,28 @@
+void main() {
+  var gifts = Map<String, String>();
+  gifts['first'] = 'partridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+  gifts['nama'] = 'Fridolf Berlin Torotodo Zega';
+  gifts['nim'] = '20235520003';
+
+  var nobleGases = Map<int, String>();
+  nobleGases[2] = 'helium';
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+  nobleGases[20] = 'Fridolf Berlin Torotodo Zega';
+  nobleGases[21] = '20235520003';
+
+  var mhs1 = Map<String, String>();
+  mhs1['nama'] = 'Fridolf Berlin Torotodo Zega';
+  mhs1['nim'] = '20235520003';
+
+  var mhs2 = Map<int, String>();
+  mhs2[1] = 'Fridolf Berlin Torotodo Zega';
+  mhs2[2] = '20235520003';
+
+  print('gifts: $gifts');
+  print('nobleGases: $nobleGases');
+  print('mhs1: $mhs1');
+  print('mhs2: $mhs2');
+}
